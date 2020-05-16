0e84f3f96095e081c5a795d5f1fecfe0b393a696
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2020-05-16 12:03:32 +0200

    Add etna.yaml with demo parameters

--- service_composition/python_services/get_tweets.py
+++ service_composition/python_services/get_tweets.py
@@ -3,6 +3,6 @@ import json
 def run(e = None, **kwargs):
     from service_composition.mini_crawler.fake_crawler import FakeCrawler
 
-    crawler = FakeCrawler(data_path="data/tweets/default.txt")
+    crawler = FakeCrawler(data_path="data/tweets/default-etna.txt")
     tweets = crawler.get_tweets()
     return [json.dumps(t) for t in tweets]
